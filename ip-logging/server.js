3a321ac5d49145d3d186b92f737b10aed8a19f84
BEGA <hackersnight.bega.25@gmail.com>
2025-06-14 11:25:41 +0000

    Added new Spiele in n8n Form

--- ip-logging/server.js
+++ ip-logging/server.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 
 const PORT = 3005; // Port für das Logging-Skript
 const LOGFILE = './ip_log.txt'; // Pfad zur Log-Datei
-const FORMULAR_URL = 'http://192.168.100.73:5678/form/b5b09eca-2b69-43bf-8240-ba4a063d219e'; // Ziel-URL für Weiterleitung
+const FORMULAR_URL = 'http://192.168.100.73:5678/form/e1d1b9c8-f46f-4ca8-8061-f0a317e1964e'; // Ziel-URL für Weiterleitung
 
 http.createServer((req, res) => {
   // IP-Adresse ermitteln
